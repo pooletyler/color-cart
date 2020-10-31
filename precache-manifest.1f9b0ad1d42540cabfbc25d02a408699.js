7e6428de0bfd529bde034eb0a3ce3a18d104b49b
Tyler Poole <tylerpoole@Tylers-Mac-mini.local>
2020-10-31 02:53:44 -0400

    Updates

---
+++ precache-manifest.1f9b0ad1d42540cabfbc25d02a408699.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17e154e39bfb3f89c7cc9d702a38496",
+    "url": "/color-cart/index.html"
+  },
+  {
+    "revision": "02dbca22a321d0c000a3",
+    "url": "/color-cart/static/css/main.3c9cd770.chunk.css"
+  },
+  {
+    "revision": "fb0c66436c1a733280e6",
+    "url": "/color-cart/static/js/2.1b38e4dd.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/color-cart/static/js/2.1b38e4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dbca22a321d0c000a3",
+    "url": "/color-cart/static/js/main.f85e8479.chunk.js"
+  },
+  {
+    "revision": "20d9fbf52976d4873847",
+    "url": "/color-cart/static/js/runtime-main.39c89043.js"
+  },
+  {
+    "revision": "39a606266fd81d8ed5dd7cc3045ae5f4",
+    "url": "/color-cart/static/media/cart.39a60626.svg"
+  },
+  {
+    "revision": "56b04b4cd009dad35fefe606dc0e8d64",
+    "url": "/color-cart/static/media/newEngen.56b04b4c.svg"
+  },
+  {
+    "revision": "6068cc1fac0c207a2bf78cab7913dd71",
+    "url": "/color-cart/static/media/trashCan.6068cc1f.svg"
+  }
+]);
